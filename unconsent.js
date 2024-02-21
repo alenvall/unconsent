ffdb0ee1f15896dc319cf5754c603abb8b9ab1ea
Damoon Rashidi <damoon.rashidi@gmail.com>
2024-02-21 15:54:47 +0100

    Initial commit

---
+++ unconsent.js
@@ -0,0 +1,46 @@
+const bodyObserver = new MutationObserver(() => {
+  const forbidden = [
+    "onetrust",
+    "sp_",
+    "didomi",
+    "cookie",
+    "consent",
+    "fides",
+    "ch2",
+    "termly",
+  ];
+  if (document.body.style.overflow === "hidden") {
+    document.body.style.overflow = "unset";
+  }
+
+  document.querySelectorAll("div").forEach((div) => {
+    const id = div.getAttribute("id");
+    if (id && forbidden.some((f) => id.includes(f))) {
+      try {
+        div.parentNode.removeChild(div);
+      } catch {}
+      return;
+    }
+
+    const classes = div.getAttribute("class");
+    if (!classes) {
+      return;
+    }
+
+    if (forbidden.some((word) => classes.toLowerCase().includes(word))) {
+      try {
+        div.parentNode.removeChild(div);
+      } catch {}
+    }
+  });
+});
+
+bodyObserver.observe(document.body, {
+  childList: true,
+  subtree: true,
+  attributes: true,
+});
+
+setTimeout(() => {
+  bodyObserver.disconnect();
+}, 5000);
